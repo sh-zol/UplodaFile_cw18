62586dd77eb56fd05b8092e199c9f1e02f4d48f2
maleki <maleki@MASOUD>
2024-02-23 09:30:04 +0330

    Add project files.

---
+++ UplodaFile/Pages/Index.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace UplodaFile.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public IFormFile File { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        [HttpPost]
+        public IActionResult OnPost()
+        {
+            if (File != null && File.Length > 0)
+            {
+                var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/uploads", File.FileName);
+
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                     File.CopyToAsync(stream);
+                }
+
+                return RedirectToPage("/Index");
+            }
+
+            return Page();
+        }
+    }
+}
